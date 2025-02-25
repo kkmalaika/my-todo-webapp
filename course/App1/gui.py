37053eb4237102fb8ec6130900595d7d9b3aa742
Kelly <kkmalaika@yahoo.com>
2025-02-25 16:13:30 +0100

    Implement the Add Button, Day 17

--- course/App1/gui.py
+++ course/App1/gui.py
@@ -1,13 +1,28 @@
-import FreeSimpleGUI as sg
+import FreeSimpleGUI  as sg
 import functions
+#from course.App1.cli.py import new_todo
 
 label = sg.Text("Veuillez saisir une tâche")
-input_box = sg.InputText(tooltip="Saisir une tâche")
+input_box = sg.InputText(tooltip="Saisir une tâche", key="todo")
 add_button = sg.Button("Ajouter")
 
-window = sg.Window('Bonjour Kelly, Voici vos tâches du jour', layout=[[label], [input_box, add_button] ])
-window.read()
-print("Coucou")
+window = sg.Window('Bonjour Kelly, Voici vos tâches du jour',
+                   layout=[[label], [input_box, add_button]],
+                   font=('Helvetica', 20))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Ajouter":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
 
 
