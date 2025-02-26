d814f5d6faf5d3401e0993cdc52e10d23be4e96d
Kelly <kkmalaika@yahoo.com>
2025-02-26 15:07:16 +0100

    Implement the Edit Button, Day 17

--- course/App1/gui.py
+++ course/App1/gui.py
@@ -5,9 +5,12 @@ import functions
 label = sg.Text("Veuillez saisir une tâche")
 input_box = sg.InputText(tooltip="Saisir une tâche", key="todo")
 add_button = sg.Button("Ajouter")
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=True, size=[45, 10])
+edit_button = sg.Button("Modifier")
 
 window = sg.Window('Bonjour Kelly, Voici vos tâches du jour',
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
                    font=('Helvetica', 20))
 
 while True:
@@ -20,6 +23,17 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Modifier":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo'] + "\n"
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
 
