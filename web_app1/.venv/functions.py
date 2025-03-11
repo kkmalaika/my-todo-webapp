5530e6049fb6e8e58d69401be37f948c998de415
Kelly <kkmalaika@yahoo.com>
2025-03-11 14:14:53 +0100

    Initial commit

---
+++ web_app1/.venv/functions.py
@@ -0,0 +1,19 @@
+FILEPATH = "Ma_Todo_Quotidienne.txt"
+def get_todos(filepath = FILEPATH):
+    """ouvrir ma todo
+     montrer la liste"""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+def write_todos(todos_arg, filepath = FILEPATH):
+    """Ecrire dans ma liste todo et
+    dans le fichier txt """
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_arg)
+
+# print(__name__)
+
+if __name__ == "__main__" :
+    print ("Hello from functions")
+    print(get_todos())
