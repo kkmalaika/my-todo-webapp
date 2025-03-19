0ec6bae3a4a4df149abc81d138b2ce0c18da71eb
Kelly <kkmalaika@yahoo.com>
2025-03-19 23:34:52 +0100

    correct functions

--- functions.py
+++ functions.py
@@ -1,14 +1,12 @@
 FILEPATH = "Ma_Todo_Quotidienne.txt"
 def get_todos(filepath = FILEPATH):
-    """ouvrir ma todo
-     montrer la liste"""
+
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 def write_todos(todos_arg, filepath = FILEPATH):
-    """Ecrire dans ma liste todo et
-    dans le fichier txt """
+
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
 
